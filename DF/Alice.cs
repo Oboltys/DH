322810e89776efe2d8c7e69785782b6192f7a7e6
Pavel <Pavel.Ivanchenko@studentpartner.com>
2013-07-12 14:24:43 +0700

    Diffie-Hellman, DH

---
+++ DF/Alice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DF
+{
+    class Alice
+    {
+        public static byte[] alicePublicKey;
+
+        public static void Main(string[] args)
+        {
+            using (ECDiffieHellmanCng alice = new ECDiffieHellmanCng())
+            {
+
+                alice.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                alice.HashAlgorithm = CngAlgorithm.Sha256;
+                alicePublicKey = alice.PublicKey.ToByteArray();
+                Bob bob = new Bob();
+                CngKey k = CngKey.Import(bob.bobPublicKey, CngKeyBlobFormat.EccPublicBlob);
+                byte[] aliceKey = alice.DeriveKeyMaterial(CngKey.Import(bob.bobPublicKey, CngKeyBlobFormat.EccPublicBlob));
+                byte[] encryptedMessage = null;
+                byte[] iv = null;
+                Send(aliceKey, "Secret message", out encryptedMessage, out iv);
+                bob.Receive(encryptedMessage, iv);
+            }
+
+        }
+
+        private static void Send(byte[] key, string secretMessage, out byte[] encryptedMessage, out byte[] iv)
+        {
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                iv = aes.IV;
+
+                // Encrypt the message
+                using (MemoryStream ciphertext = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ciphertext, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] plaintextMessage = Encoding.UTF8.GetBytes(secretMessage);
+                    cs.Write(plaintextMessage, 0, plaintextMessage.Length);
+                    cs.Close();
+                    encryptedMessage = ciphertext.ToArray();
+                }
+            }
+        }
+
+    }
+
+}
---
+++ DF/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {        
+
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+         
+                // Decrypt the message
+                using (MemoryStream plaintext = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(plaintext, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(encryptedMessage, 0, encryptedMessage.Length);
+                        cs.Close();
+                        string message = Encoding.UTF8.GetString(plaintext.ToArray());
+                        Console.WriteLine(message);
+
+                        label1.Text = Encoding.UTF8.GetString(Alice.alicePublicKey);
+                    }
+                }
+            }
+        }
+    }
+}
